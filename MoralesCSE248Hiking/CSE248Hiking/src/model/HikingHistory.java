69e3308b21e9fcb421ae94cff354e41e57b059d3
Peter Morales <petermorales717@gmail.com>
2021-02-10 11:33:52 -0500

    model

---
+++ MoralesCSE248Hiking/CSE248Hiking/src/model/HikingHistory.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.LinkedList;
+
+public class HikingHistory {
+	
+	private LinkedList<Trail> hikingHistory = new LinkedList<>();
+	
+	public HikingHistory() {
+		
+	}
+}
